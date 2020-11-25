6cedb53fcab68de241068d2a69793ff37cdf241d
Ui Sam dos Santos <50969620+uisam00@users.noreply.github.com>
2020-11-25 14:35:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res )=>{
     res.status(200).send({message: 'Olá mundo!'})
 })
 
+app.get('/ola', (req, res )=>{
+    res.status(200).send({message: 'Olá mundo! 2'})
+})
+
 app.listen(3001, ()=>{
     console.log('Api rodando no porto 3001');
 })
